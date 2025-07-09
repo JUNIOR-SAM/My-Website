d8a24966ce46c7c03fa4ca7305af81d464c0ed5a
samuel <oyebodes19@gmail.com>
2025-07-09 17:08:40 +0100

    i add form

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 const myEmail = () => {
     form.style.display = "block";
 }
+const exitEmail = () => {
+    form.style.display = "none";
+}
+
+const nameInput = text.value;
+
+const allowEmail = () => {
+    if (nameInput === "" || email === "" || message === "") {
+        alert("Please fill all the fields");
+        return;
+    }
+}
