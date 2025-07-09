dfd4ca6304fea2b460984b51c133823f67478918
samuel <oyebodes19@gmail.com>
2025-07-09 17:11:15 +0100

    i remove small javascript

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const exitEmail = () => {
     form.style.display = "none";
 }
 
-const nameInput = text.value;
+// const nameInput = text.value;
 
-const allowEmail = () => {
-    if (nameInput === "" || email === "" || message === "") {
-        alert("Please fill all the fields");
-        return;
-    }
-}
+// const allowEmail = () => {
+//     if (nameInput === "" || email === "" || message === "") {
+//         alert("Please fill all the fields");
+//         return;
+//     }
+// }
